bbad4ef721fa9dc02a3b9c0d607478f3855069b9
Khue Vu <khuevu@gmail.com>
2012-09-16 19:35:07 +0800

    project setup

--- README.md
+++ README.md
@@ -2,3 +2,7 @@ welog
 =====
 
 A web service to log chat on irc (and more)
+
+Installation 
+============
+Run ./setup_virtualenv.sh
---
+++ setup_virtualenv.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+command -v mkvirtualenv >/dev/null && virtual_env_installed=true | virtual_env_installed=false
+
+if ! $virtual_env_installed ; then
+    sudo pip install virtualenv
+    sudo pip install virtualenvwrapper
+    export WORKON_HOME=$HOME/.virtualenvs
+    source /usr/local/bin/virtualenvwrapper.sh
+    export PIP_VIRTUALENV_BASE=$WORKON_HOME
+    export PIP_RESPECT_VIRTUALENV=true
+fi
+
+mkvirtualenv -p python2.7 welog
+workon welog
+pip install -r requirements.txt
